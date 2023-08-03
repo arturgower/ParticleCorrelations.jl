530cb51ce9aaba6228e51ce2e0baa32b440dfea6
Artur Gower <arturgower@gmail.com>
2023-08-03 17:05:16 +0100

    move essential types and functions from EffectiveWaves to this package

--- src/ParticleCorrelations.jl
+++ src/ParticleCorrelations.jl
@@ -1,5 +1,10 @@
+@reexport using MultipleScattering: PhysicalMedium, Acoustic, Shape, Sphere, Circle
+@reexport using EffectiveWaves: Specie, Species
+
 # module ParticleCorrelations
 
 # Write your package code here.
 
 # end
+
+included("pair-correlations.jl")
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,4 @@
 using ParticleCorrelations
 using Test
 
-@testset "ParticleCorrelations.jl" begin
-    # Write your tests here.
-end
+include("pair-correlations.jl")
